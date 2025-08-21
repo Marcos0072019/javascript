9a791e78a6abaffa5fb40ce2289a12f7fbbb92dd
Marcos0072019 <mar0072019@gmail.com>
2025-08-21 16:03:25 -0300

    Add files via upload

---
+++ idade/script.js
@@ -0,0 +1,12 @@
+function mostrarmensagem()
+{
+
+    let idade = prompt("Qual sua idade?");
+    if(idade<=17)
+    {
+        alert("você é menor de idade");
+    }else
+    {
+        alert("você é maior de idade");
+    }
+}
